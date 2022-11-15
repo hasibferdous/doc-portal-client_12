dd66b6182ec98800fe87fcabe027318b1ad74b8b
hasibferdous <hasibferdous.ict@gmail.com>
2022-11-16 00:19:28 +0600

    AuthProvider added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import router from './Routes/Routes/Routes';
 import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <div className='max-w-[1440px] mx-auto'>
       <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
