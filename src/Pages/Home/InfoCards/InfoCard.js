7a2fdc7b73f7588076b55f79992d9fa91b6eda5a
hasibferdous <hasibferdous.ict@gmail.com>
2022-11-23 02:47:41 +0600

    adding and managing doctor

--- src/Pages/Home/InfoCards/InfoCard.js
+++ src/Pages/Home/InfoCards/InfoCard.js
@@ -3,12 +3,12 @@ const InfoCard = ({card}) => {
     const { name, description, icon, bgClass} = card;
     return (
         <div>
-            <div class={`card text-white p-6 md:card-side shadow-xl ${bgClass}`}>
+            <div className={`card text-white p-6 md:card-side shadow-xl ${bgClass}`}>
                 <figure>
                     <img src={icon} alt="Movie"/>
                 </figure>
-            <div class="card-body">
-                <h2 class="card-title">{name}</h2>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
                 <p>{description}</p>
             </div>
             </div>
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -3,7 +3,9 @@ import DashboardLayout from "../../Layout/DashboardLayout";
 import Main from "../../Layout/Main/Main";
 
 import Appointment from "../../Pages/Appointment/Appointment/Appointment";
+import AddDoctor from "../../Pages/Dashboard/AddDoctor/AddDoctor";
 import AllUsers from "../../Pages/Dashboard/AllUsers/AllUsers";
+import ManageDoctors from "../../Pages/Dashboard/ManageDoctors/ManageDoctors";
 
 import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../../Pages/Home/Home/Home";
@@ -46,6 +48,14 @@ const router = createBrowserRouter([
             {
                 path: '/dashboard/allusers',
                 element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
+            },
+            {
+                path: '/dashboard/adddoctor',
+                element: <AdminRoute><AddDoctor></AddDoctor></AdminRoute>
+            },
+            {
+                path: '/dashboard/managedoctors',
+                element: <AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
             },
         ]
     }
